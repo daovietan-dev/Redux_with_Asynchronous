6d1e5d9188148627d1dc24344794922f6e4e707e
Việt An <111722238+daovietan-dev@users.noreply.github.com>
2023-09-24 15:59:09 +0700

    locate asynchronous code to actionCreator function
    instead of dispatch immediately an action obj, I dispatch a function that really dispatch an action obj latter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import { useEffect } from 'react'
-import { uiActions } from './store/ui-slice'
+import { sendCartData, uiActions } from './store/ui-slice'
 import Notification from './components/UI/Notification'
 
 let isInitial = true
@@ -16,50 +16,12 @@ function App() {
 
   useEffect(
     prams => {
-      const sendCartData = async params => {
-        dispatch(
-          uiActions.showNotification({
-            status: 'pending',
-            title: 'Sending...',
-            message: 'Sending cart data!',
-          })
-        )
-
-        const response = await fetch(
-          'https://redux-learn-8e504-default-rtdb.firebaseio.com/cart.json',
-          {
-            method: 'PUT',
-            body: JSON.stringify(cart),
-          }
-        )
-
-        if (!response.ok) {
-          throw new Error('Somethings wrong!')
-        }
-
-        dispatch(
-          uiActions.showNotification({
-            status: 'success',
-            title: 'Success!',
-            message: 'Sending cart data successfully!',
-          })
-        )
-      }
-
       if (isInitial) {
         isInitial = false
         return
       }
-
-      sendCartData().catch(error => {
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error!',
-            message: 'Sending cart data failed!',
-          })
-        )
-      })
+      // sendCartData(cart) là một actionCreator nhưng lại return một hàm. Tuy nhiên, điều này là khả thi vì Redux chấp nhận nó và sẽ thực thi hàm trả về đó một lần nữa
+      dispatch(sendCartData(cart))
     },
     [cart, dispatch]
   )
