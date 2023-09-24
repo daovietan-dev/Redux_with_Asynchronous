d078c3ad03d7f32e703957bf70bb75c24ecacf39
Việt An <111722238+daovietan-dev@users.noreply.github.com>
2023-09-25 00:50:07 +0700

    update fetch data in the first render
    fetch cart's items in database

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import { useEffect } from 'react'
-import { sendCartData, uiActions } from './store/ui-slice'
+import { fetchCartData, sendCartData } from './store/cart-action'
 import Notification from './components/UI/Notification'
 
 let isInitial = true
@@ -12,16 +12,25 @@ function App() {
   const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart)
+
   const notification = useSelector(state => state.ui.notification)
+
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [])
 
   useEffect(
     prams => {
       if (isInitial) {
+        // dispatch(sendCartData(cart))
         isInitial = false
         return
       }
+
       // sendCartData(cart) là một actionCreator nhưng lại return một hàm. Tuy nhiên, điều này là khả thi vì Redux chấp nhận nó và sẽ thực thi hàm trả về đó một lần nữa
-      dispatch(sendCartData(cart))
+      if (cart.changed) {
+        dispatch(sendCartData(cart))
+      }
     },
     [cart, dispatch]
   )
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -17,54 +17,5 @@ const uiSlice = createSlice({
   },
 })
 
-export const sendCartData = cart => {
-  // dispatch argument được tạo tự động, vì đây là một thiết kế của Redux giúp cho những developer có thể thực thi side-effect trong actionCreator()
-  return async dispatch => {
-    // ta thực sự đã gửi đi một action khác trong hàm trả về này
-    dispatch(
-      uiSlice.actions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    )
-
-    // khai báo asynchronous code
-    const sendRequest = async () => {
-      const response = await fetch(
-        'https://redux-learn-8e504-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      )
-
-      if (!response.ok) {
-        throw new Error('Somethings wrong!')
-      }
-    }
-
-    // thực thi asynchronous code
-    try {
-      await sendRequest()
-      dispatch(
-        uiSlice.actions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sending cart data successfully!',
-        })
-      )
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      )
-    }
-  }
-}
-
 export default uiSlice
 export const uiActions = uiSlice.actions
